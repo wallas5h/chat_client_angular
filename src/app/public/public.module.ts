038ace0770bd789befadc73a1bd9f627526d407a
wallas5h@gmail.com <wallas5h@gmail.com>
2022-12-04 23:25:38 +0100

    add public module

---
+++ src/app/public/public.module.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { LoginComponent } from "./components/login/login.component";
+import { RegisterComponent } from "./components/register/register.component";
+import { PublicRoutingModule } from "./public-routing.module";
+
+@NgModule({
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, PublicRoutingModule],
+})
+export class PublicModule {}
