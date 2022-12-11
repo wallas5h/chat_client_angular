ce3449184f503ceb4957f81a3ba7ac7bace1409c
wallas5h@gmail.com <wallas5h@gmail.com>
2022-12-11 23:16:14 +0100

    update login component

--- src/app/public/components/login/login.component.ts
+++ src/app/public/components/login/login.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "src/app/services/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styles: [
-  ]
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styles: [],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    localStorage.clear();
   }
 
+  imageBgUrl =
+    "https://images.pexels.com/photos/3178818/pexels-photo-3178818.jpeg?auto=compress&cs=tinysrgb&w=640&h=960&dpr=1";
+
+  loginForms = new FormGroup({
+    email: new FormControl(
+      "",
+      Validators.compose([Validators.email, Validators.required])
+    ),
+    password: new FormControl("", Validators.required),
+  });
+
+  async loginUser() {
+    if (this.loginForms.valid) {
+      await this.authService.login({
+        email: String(this.loginForms.value.email),
+        password: String(this.loginForms.value.password),
+      });
+    }
+  }
 }
