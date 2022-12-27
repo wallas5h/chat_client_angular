d1d541b92631b25f83b7f9405f2ae050f8447c8a
wallas5h@gmail.com <wallas5h@gmail.com>
2022-12-27 19:18:19 +0100

    add token interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./components/header/header.component";
 import { MaterialModule } from "./Material-Module";
+import { HomeComponent } from "./pages/home/home.component";
 import { AuthService } from "./services/auth.service";
-import { HeaderComponent } from './components/header/header.component';
-import { HomeComponent } from './pages/home/home.component';
+import { ChatService } from "./services/chat.service";
+import { SocketioService } from "./services/socketio.service";
+import { TokenInterceptorService } from "./services/token-interceptor.service";
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent, HomeComponent],
@@ -19,7 +22,16 @@ import { HomeComponent } from './pages/home/home.component';
     MaterialModule,
     HttpClientModule,
   ],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    ChatService,
+    SocketioService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptorService,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/services/token-interceptor.service.ts
@@ -0,0 +1,27 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TokenInterceptorService implements HttpInterceptor {
+  constructor() {}
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    let jwttoken = req.clone({
+      setHeaders: {
+        Authorization: "Bearer" + localStorage.getItem("token"),
+      },
+    });
+
+    return next.handle(jwttoken);
+  }
+}
