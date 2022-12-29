390b6f5aec0a3fb81fa4d6dc343527f2c57ad1e1
wallas5h@gmail.com <wallas5h@gmail.com>
2022-12-29 19:37:15 +0100

    create variable isAuthenticated

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -9,6 +9,7 @@ import {
 } from "@angular/router";
 import { Observable } from "rxjs";
 import { AuthService } from "../services/auth.service";
+import { UserEntity } from "../types/user";
 
 @Injectable({
   providedIn: "root",
@@ -20,6 +21,9 @@ export class AuthGuard implements CanActivate {
     private _snackBar: MatSnackBar
   ) {}
 
+  isAuthenticated: Boolean = false;
+  user: UserEntity | undefined;
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
@@ -28,7 +32,10 @@ export class AuthGuard implements CanActivate {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    if (!this.authService.isAuthenticated) {
+    this.user = this.authService.getUserData();
+    this.isAuthenticated = this.user ? true : false;
+
+    if (!this.isAuthenticated) {
       this.router.navigate(["/login"]);
       this._snackBar.open("Login first", "Ok", { duration: 3000 });
 
