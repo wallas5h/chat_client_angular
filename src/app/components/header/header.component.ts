ff53bf4a6f252319f49e114ed13d0769d6ac531c
wallas5h@gmail.com <wallas5h@gmail.com>
2022-12-29 19:35:29 +0100

    add image after login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, DoCheck, OnChanges, OnInit } from "@angular/core";
 import { AuthService } from "src/app/services/auth.service";
+import { UserEntity } from "src/app/types/user";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styles: [],
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges, DoCheck {
   constructor(private authService: AuthService) {}
+
+  isAuthenticated: Boolean = false;
+  user: UserEntity | undefined;
+
+  ngDoCheck(): void {
+    this.user = this.authService.getUserData();
+    this.isAuthenticated = this.user ? true : false;
+  }
+  ngOnChanges(): void {}
 
   ngOnInit(): void {}
 
-  isAuthenticated = this.authService.isAuthenticated;
+  logout() {
+    this.authService.logout();
+  }
 }
