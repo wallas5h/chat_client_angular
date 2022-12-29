e1f7f58e8768d664ee9fbcc29410c67c329c77de
wallas5h@gmail.com <wallas5h@gmail.com>
2022-12-29 19:34:37 +0100

    add socket.io to appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from "@angular/core";
+import { Component, OnChanges, OnDestroy, OnInit } from "@angular/core";
+import { AuthService } from "./services/auth.service";
+import { SocketioService } from "./services/socketio.service";
+import { UserEntity } from "./types/user";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styles: [],
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy, OnChanges {
   title = "chat";
+  user: UserEntity | undefined;
+
+  constructor(
+    private socketioService: SocketioService,
+    private authService: AuthService
+  ) {}
+
+  ngOnChanges(): void {
+    this.user = this.authService.getUserData();
+    console.log(this.user);
+  }
+
+  ngOnInit() {
+    this.socketioService.setupSocketConnection();
+    // this.user = this.authService.getUserData();
+  }
+
+  ngOnDestroy(): void {
+    this.socketioService.disconnect();
+    // localStorage.clear();
+  }
 }
