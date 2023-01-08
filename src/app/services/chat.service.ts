ad7dc4d8dfb9128d3acad8f06bee79cef2f14c05
wallas5h@gmail.com <wallas5h@gmail.com>
2023-01-08 12:57:23 +0100

    update chatService: create method createRoom, dislikeRomm

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,18 +1,50 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import axios from "axios";
 import { apiUrl } from "src/config/api";
+import { AuthService } from "./auth.service";
+import { SocketioService } from "./socketio.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class ChatService {
-  constructor(private http: HttpClient) {}
+  user = this.authService.user;
+
+  constructor(
+    private http: HttpClient,
+    private socketioService: SocketioService,
+    private authService: AuthService
+  ) {}
 
   getRooms() {
     return this.http.get(apiUrl + "/rooms");
   }
 
-  getMembers() {
-    return this.http.get(apiUrl + "/rooms");
+  async getMembers() {
+    return this.authService.getUsers();
+  }
+
+  async createRoom(name: string, type: string) {
+    let room = {
+      name,
+      type,
+    };
+    const req = this.http.post(`${apiUrl}/rooms`, room);
+    req.subscribe((res) => {
+      // console.log(res);
+    });
+    return req;
+  }
+
+  async dislikeRoom(roomId: string) {
+    const body = {
+      id: roomId,
+    };
+    axios.patch(`${apiUrl}/rooms`, body);
+  }
+
+  async deleteRoom(roomId: string) {
+    axios.delete(`${apiUrl}/rooms/${roomId}`);
   }
 }
