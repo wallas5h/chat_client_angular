0fb40c616c4e323627498243a013518ce5b213e8
wallas5h@gmail.com <wallas5h@gmail.com>
2023-01-08 12:59:10 +0100

    add popup editRomm

---
+++ src/app/components/editroompopup/editroompopup.component.ts
@@ -0,0 +1,59 @@
+import { CommonModule } from "@angular/common";
+import { Component, Inject, OnInit } from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MaterialModule } from "src/app/Material-Module";
+import { AuthService } from "src/app/services/auth.service";
+
+@Component({
+  selector: "app-editroompopup",
+  standalone: true,
+  imports: [
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FormsModule,
+  ],
+  templateUrl: "./editroompopup.component.html",
+  styles: [],
+})
+export class EditroompopupComponent implements OnInit {
+  privacyOptions = ["public", "private"];
+
+  roomForm = new FormGroup({
+    name: new FormControl(this.data.room.name),
+    type: new FormControl(this.data.room.type),
+  });
+
+  addUserForm = new FormGroup({
+    username: new FormControl("", Validators.required),
+  });
+
+  members = this.data.room.members;
+  user = this.authService.user;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private ref: MatDialogRef<EditroompopupComponent>,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit(): void {}
+
+  updateRoom() {
+    console.log(this.roomForm.value);
+  }
+
+  inviteUserToRoom() {
+    // console.log(name, id);
+    console.log(this.addUserForm.value.username);
+    this.addUserForm.value.username = "";
+  }
+}
