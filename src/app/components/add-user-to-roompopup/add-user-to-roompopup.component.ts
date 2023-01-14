8040a57e89ce4cf1bf6eeb7f6130fe2a02f58b5b
wallas5h@gmail.com <wallas5h@gmail.com>
2023-01-14 13:23:49 +0100

    add method, feature findUser and addUserToRoom

--- src/app/components/add-user-to-roompopup/add-user-to-roompopup.component.ts
+++ src/app/components/add-user-to-roompopup/add-user-to-roompopup.component.ts
@@ -8,8 +8,11 @@ import {
   Validators,
 } from "@angular/forms";
 import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import { MatSnackBar } from "@angular/material/snack-bar";
 import { MaterialModule } from "src/app/Material-Module";
 import { AuthService } from "src/app/services/auth.service";
+import { ChatService } from "src/app/services/chat.service";
+import { UserFindResponse } from "src/app/types/user";
 
 @Component({
   selector: "app-add-user-to-roompopup",
@@ -25,35 +28,61 @@ import { AuthService } from "src/app/services/auth.service";
   styles: [],
 })
 export class AddUserToRoompopupComponent implements OnInit {
-  privacyOptions = ["public", "private"];
-
-  roomForm = new FormGroup({
-    name: new FormControl(this.data.room.name),
-    type: new FormControl(this.data.room.type),
-  });
-
   addUserForm = new FormGroup({
     username: new FormControl("", Validators.required),
   });
 
+  room = this.data.room;
   members = this.data.room.members;
   user = this.authService.user;
+  findUserRes: UserFindResponse[] | [] | undefined = [];
+  findUserFailed: boolean | undefined;
+  formUsernameLength = 0;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private ref: MatDialogRef<AddUserToRoompopupComponent>,
-    private authService: AuthService
+    private authService: AuthService,
+    private chatService: ChatService,
+    private _snackBar: MatSnackBar
   ) {}
 
   ngOnInit(): void {}
 
-  updateRoom() {
-    console.log(this.roomForm.value);
-  }
+  async findUser() {
+    if (!this.addUserForm.value.username) return;
 
-  inviteUserToRoom() {
-    // console.log(name, id);
-    console.log(this.addUserForm.value.username);
+    this.authService
+      .findUserByName(this.addUserForm.value.username)
+      ?.then((res) => {
+        if (res.status === 200) {
+          this.findUserRes = res.data.users;
+          this.findUserFailed = false;
+        }
+      })
+      .catch((err) => {
+        this.findUserFailed = true;
+      });
+
     this.addUserForm.value.username = "";
   }
+
+  addUserToRoom = (id: string, name: string) => {
+    this.chatService
+      .addUserToRoom(this.room._id, id, name)
+      .then((res) => {
+        if (res.status === 200) {
+          this._snackBar.open(`${name} added to room`, "Ok", {
+            duration: 3000,
+          });
+        } else if (res.status === 302) {
+          this._snackBar.open(`${name} just exist in room`, "Ok", {
+            duration: 3000,
+          });
+        }
+      })
+      .catch((err) => {
+        this._snackBar.open(`${err}`, "Ok", { duration: 3000 });
+      });
+  };
 }
