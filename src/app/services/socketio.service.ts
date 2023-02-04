18d97f9b0c589ae67fb11183dddc4f92d67cf2ae
wallas5h@gmail.com <wallas5h@gmail.com>
2023-02-04 21:36:12 +0100

    add method's to get and set number of new messages

--- src/app/services/socketio.service.ts
+++ src/app/services/socketio.service.ts
@@ -2,14 +2,20 @@ import { Injectable } from "@angular/core";
 import { io } from "socket.io-client";
 import { apiSocketUrl } from "src/config/api";
 
-// export const socket = io(apiSocketUrl);
-export const socket = io("http://localhost:3001");
+export const socket = io(apiSocketUrl);
+// export const socket = io("http://localhost:3001");
+
+export interface Dictionary<Value> {
+  [param: string]: Value;
+}
 
 @Injectable({
   providedIn: "root",
 })
 export class SocketioService {
   socket: any;
+  newMessages: Dictionary<number> =
+    JSON.parse(localStorage.getItem("newMessages")!) ?? {};
 
   constructor() {}
 
@@ -22,4 +28,43 @@ export class SocketioService {
       this.socket.disconnect();
     }
   }
+
+  getNumberOfNewMessages() {
+    let messagesJson = localStorage.getItem("newMessages")!;
+    this.newMessages = JSON.parse(messagesJson);
+    return JSON.parse(messagesJson);
+  }
+
+  setNumberOfNewMessages() {
+    socket.off("notifications").on("notifications", (room) => {
+      if (this.newMessages[room]) {
+        this.newMessages[room] += 1;
+        localStorage.setItem("newMessages", JSON.stringify(this.newMessages));
+      } else {
+        this.newMessages[room] = 1;
+        localStorage.setItem("newMessages", JSON.stringify(this.newMessages));
+      }
+    });
+  }
+
+  deleteNotification(roomId: string) {
+    const msgNotification: Dictionary<number> = JSON.parse(
+      localStorage.getItem("newMessages")!
+    );
+    delete msgNotification[roomId];
+    this.newMessages = msgNotification;
+    localStorage.setItem("newMessages", JSON.stringify(this.newMessages));
+  }
+
+  // getMessagesFromRoom(): MessageResponseDto[] | []  {
+  //   let messages;
+  //   try {
+  //     socket.off("room-messages").on("room-messages", (roomMessages) => {
+  //       messages= roomMessages;
+  //     });
+  //   } catch (error) {
+  //     messages= [];
+  //   }
+  //   return messages
+  // }
 }
