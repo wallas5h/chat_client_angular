18c1fcdad4b61baed32931c65c3d40bee67df212
wallas5h@gmail.com <wallas5h@gmail.com>
2023-03-04 18:13:00 +0100

    add and instal fontAwersome

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { HomePhoneContentComponent } from "./components/home-phone-content/home-phone-content.component";
 import { MaterialModule } from "./Material-Module";
 import { ChatRoutingModule } from "./pages/chat/chat-routing.module";
 import { HomeComponent } from "./pages/home/home.component";
@@ -14,12 +16,17 @@ import { ChatService } from "./services/chat.service";
 import { SocketioService } from "./services/socketio.service";
 import { TokenInterceptorService } from "./services/token-interceptor.service";
 import { UploadService } from "./services/upload.service";
-import { HomePhoneContentComponent } from './components/home-phone-content/home-phone-content.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, HomePhoneContentComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    HomePhoneContentComponent,
+  ],
   imports: [
     BrowserModule,
+    FontAwesomeModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
