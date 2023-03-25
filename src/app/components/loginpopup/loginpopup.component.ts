fed5396dd318ede39b4a8686d8c00588715fd6cc
wallas5h@gmail.com <wallas5h@gmail.com>
2023-03-25 19:32:33 +0100

    add method to resend email confirmation

--- src/app/components/loginpopup/loginpopup.component.ts
+++ src/app/components/loginpopup/loginpopup.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from "@angular/common";
 import { Component, Inject, OnInit } from "@angular/core";
-import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from "@angular/material/dialog";
 import { MaterialModule } from "src/app/Material-Module";
+import { ResendEmailVerificatonPopupComponent } from "../resend-email-verificaton-popup/resend-email-verificaton-popup.component";
 
 interface DataType {
   message: string;
-  email: string;
+  email?: string;
+  additionalInfo: boolean;
 }
 
 @Component({
@@ -18,14 +24,25 @@ interface DataType {
 export class LoginpopupComponent implements OnInit {
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: DataType,
-    public dialogRef: MatDialogRef<LoginpopupComponent>
+    public dialogRef: MatDialogRef<LoginpopupComponent>,
+    private dialog: MatDialog
   ) {}
 
-  ngOnInit(): void {
-    console.log(this.data);
-  }
+  ngOnInit(): void {}
 
   handleClickCloseBtn() {
     this.dialogRef.close();
+  }
+
+  resendEmailConfirmation() {
+    this.dialogRef.close();
+
+    let popup = this.dialog.open(ResendEmailVerificatonPopupComponent, {
+      maxHeight: "100vh",
+      maxWidth: "100vw",
+      data: {
+        email: this.data.email,
+      },
+    });
   }
 }
